2d22d5d5287a37184e2b5a531cb85973dc4b86bc
Noor Aldeen <knooraldeen2006@hotmail.com>
2022-11-10 14:59:09 +0300

    first-one

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './style.css';
+import "bootstrap/dist/js/bootstrap.min.js";
+import "jquery/dist/jquery.min";
+import "popper.js/dist/esm/popper.min.js";
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+var path = require("path");
+var hx = require("html-webpack-plugin");
+var cssp = require("mini-css-extract-plugin");
+var csso = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+
+    mode: 'development',
+
+    entry: {
+
+      main: path.resolve(__dirname, './src/index.js'),
+
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath:"",
+        filename: 'main.js',
+
+
+    },
+    devServer:{
+        static: {
+
+            directory: path.join(__dirname, 'dist'),
+    
+        },
+        port:1239,
+        devMiddleware: {
+            writeToDisk: true
+         },
+         hot: false,
+         liveReload: true,
+         open: true,
+    },
+    module:{
+        rules:[
+            {
+                test:/\.html$/,
+                use: [
+                        {
+                            loader: "html-loader",
+                            options: {
+                            minimize: true,
+                            },
+                        },
+                      ],
+            },
+            {
+                test: /\.css$/,
+                use: [cssp.loader, "css-loader"],
+              },
+              
+        ],
+    },
+    plugins:[
+        new hx({
+            filename:"index.html",
+            template:"./src/index.html"
+        }),
+        new cssp({
+            filename:"style.css",
+        }),
+        new csso({}),
+    ],
+
+}
