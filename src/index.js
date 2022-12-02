44d0121e2a8749849ff9a62beb38d4e1c7fc6afa
Noor Aldeen <knooraldeen2006@hotmail.com>
2022-12-02 20:26:24 +0300

    Home Page

--- src/index.js
+++ src/index.js
@@ -5,8 +5,31 @@ import "popper.js/dist/esm/popper.min.js";
 import 'webpack-jquery-ui';
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@fortawesome/fontawesome-free/js/all.min";
-
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
 $(function(){
     $('[data-bs-toggle="tooltip"]').tooltip();
+
+    $(".add-to-card").click(function(){
+        alert("أضيف المنتج إلى عربة الشراء");
+    });
+    $("#copytime").html(new Date().getFullYear());
 });
 
